4f54fa2905c738bdf5672c01412590d38b8efce8
goatess <107187023+goatess@users.noreply.github.com>
2022-07-26 14:17:11 +0200

    added some comments to class attributes

--- src/Cart.java
+++ src/Cart.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
-    private static final float BASE_DISCOUNT = 0.05f;
-    private static final int DISCOUNT_THRESHOLD = 4;
-    private float orderPrice, discount;
-    private List<Book> cart, lot, order;
+    private static final float BASE_DISCOUNT = 0.05f; // base discount is 0.05
+    private static final int DISCOUNT_THRESHOLD = 4; // number of books threshold that lead to the specified discount
+    private float orderPrice, discount; // order price and specified discount for the lot
+    private List<Book> cart, lot, order; // lists of books in cart, books in discount lot and books in final order
 
     public Cart() {
         cart = new ArrayList<>();
@@ -17,9 +17,9 @@ public class Cart {
     }
 
     public void applyDiscounts() {
-       while (cart.size() > 0) {
+        while (cart.size() > 0) {
             applyDiscount(makeLot());
-        } 
+        }
     }
 
     /** adds book to discounted lot */
@@ -40,25 +40,23 @@ public class Cart {
         float lotPrice = 0;
 
         applyBestDiscount();
-
         for (Book book : lot) {
             lotPrice += book.getPrice();
         }
 
-        lotPrice = lotPrice - (lotPrice * discount);
+        lotPrice -= (lotPrice * discount);
         orderPrice += lotPrice;
     }
 
     private void applyBestDiscount() {
-        int lotSize = lot.size();
-        int lowDiscount = lotSize - 1;
-        int highDiscount = lotSize + (lotSize - DISCOUNT_THRESHOLD);
+        int lowDiscount = lot.size() - 1;
+        int highDiscount = lot.size() + (lot.size() - DISCOUNT_THRESHOLD);
 
-        if (lotSize < DISCOUNT_THRESHOLD) {
+        if (lot.size() < DISCOUNT_THRESHOLD) {
             discount = BASE_DISCOUNT * lowDiscount;
 
-        } else if (lotSize == DISCOUNT_THRESHOLD) {
-            discount = BASE_DISCOUNT * lotSize; // = 0.2f discount
+        } else if (lot.size() == DISCOUNT_THRESHOLD) {
+            discount = BASE_DISCOUNT * lot.size();
 
         } else {
             discount = BASE_DISCOUNT * highDiscount;
